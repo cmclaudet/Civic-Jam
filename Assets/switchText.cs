b31887e88470baeb775ada704fac9a126565b2a9
Catherine Claudet <cmclaudet@gmail.com>
2017-03-25 13:34:09 +0100

    Added dialogue prefab

---
+++ Assets/switchText.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class switchText : MonoBehaviour {
+
+	public string[] lines;
+	private int currentLine;
+	private int totalLines;
+
+	// Use this for initialization
+	void Start () {
+		currentLine = 1;
+		totalLines = lines.Length;
+		GetComponent<Text>().text = lines[currentLine - 1];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("space")) {
+
+			if (currentLine < totalLines) {
+				GetComponent<Text>().text = lines[currentLine];
+				currentLine += 1;
+			} else {
+				currentLine = 1;
+				gameObject.SetActive(false);
+			}
+		}
+	}
+}
