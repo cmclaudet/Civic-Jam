32571980528f68731ef9e478d7576bda588fabda
Catherine Claudet <cmclaudet@gmail.com>
2017-03-25 17:39:34 +0100

    Added end game logic

---
+++ Assets/getFinalChoice.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class getFinalChoice : MonoBehaviour {
+	public GameObject phone;
+	public int topChoiceNumber;
+	public int bottomChoiceNumber;
+	private bool selectedTopChoice;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		selectedTopChoice = phone.GetComponent<askQuestion>().answer;
+		if (selectedTopChoice) {
+			endGameChoice.endScene = topChoiceNumber;
+		} else {
+			endGameChoice.endScene = bottomChoiceNumber;
+		}
+	}
+}
---
+++ Assets/setFinalMessage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class setFinalMessage : MonoBehaviour {
+	public string[] end4ai1;
+	public string[] end4ai2;
+	public string[] end4aii1;
+	public string[] end4aii2;
+	public string[] end4bi1;
+	public string[] end4bi2;
+	public string[] end4bii1;
+	public string[] end4bii2;
+	private string[][] allEndText;
+	private int chosenText;
+
+	// Use this for initialization
+	void Awake () {
+		allEndText = new string[8][]{end4ai1, end4ai2, end4aii1, end4aii2, end4bi1, end4bi2, end4bii1, end4bii2};
+		chosenText = endGameChoice.endScene;
+		GetComponent<switchText>().lines = allEndText[chosenText - 1];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
